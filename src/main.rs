34e5795ea54e6c6e80838b8935be7e6f89577fd0
heyesheng <supersosohime@gmail.com>
2024-11-29 11:13:03 +0800

    feat: get x-real-ip

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,33 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
+use rocket::request::{FromRequest, Outcome, Request};
+
+struct Host<'r>(&'r str);
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for Host<'r> {
+    type Error = ();
+
+    async fn from_request(req: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        match req.headers().get_one("x-real-ip") {
+            None => Outcome::Success(Host("")),
+            Some(key) => Outcome::Success(Host(key)),
+        }
+    }
+}
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[get("/<_..>")]
-fn everything() -> &'static str {
-    "I'm a teapot"
+#[get("/ip")]
+fn ip(ip: Host) -> String {
+    ip.0.to_string()
 }
 
+// 获取请求头，打印请求IP
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, everything])
+    rocket::build().mount("/", routes![index, ip])
 }
