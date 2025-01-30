921c890a6b66f979b095ee383281226575b5fa99
sosohime <supersosohime@gmail.com>
2025-01-30 18:46:01 +0800

    feat: bo tuixiu use CSR

--- apps/frontend-astro/src/components/tuixiu/bo.tsx
+++ apps/frontend-astro/src/components/tuixiu/bo.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useTuixiuCountDown, getCountDown } from '../../hooks/countDown';
-import type { CountDown } from '../../hooks/countDown';
 import { tuixiu } from '@mono/const';
 import CopyToClipboardButton from '../copyToClipboard';
 import { ArrowUpRight, ArrowRightLeft } from 'lucide-react';
@@ -8,15 +7,11 @@ import { buttonVariants } from '@mono/ui/button';
 
 const { boTuiXiuDay } = tuixiu;
 
-export default function Tuixiu({
-  initCountDown,
-}: {
-  initCountDown: CountDown;
-}) {
+export default function Tuixiu() {
   const countDown = useTuixiuCountDown(boTuiXiuDay);
 
   const getTuiClipboardText = React.useCallback(() => {
-    const botui = getCountDown(boTuiXiuDay || initCountDown);
+    const botui = getCountDown(boTuiXiuDay);
     return `普大喜奔，距离博哥退休还有${botui['yyyyMMDD hhmmss']}，转发到五个群再看你的头像，是真的！`;
   }, []);
 
@@ -24,10 +19,8 @@ export default function Tuixiu({
     <div className="text-center h-full w-full flex flex-col justify-center items-center">
       <h1 className="text-2xl pb-4">距离博哥退休还有</h1>
       <p className="text-4xl pb-4">
-        {(countDown || initCountDown)['yyyyMMDD hhmmss']}
-        <span className="text-xl pl-1">
-          {(countDown || initCountDown)['milliseconds']}
-        </span>
+        {countDown?.['yyyyMMDD hhmmss']}
+        <span className="text-xl pl-1">{countDown?.['milliseconds']}</span>
       </p>
       <div className="text-xs pb-4 scale-50">(UTC+8)</div>
 
