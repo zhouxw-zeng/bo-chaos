e35f0041725e3efc1fa2e97f4a78ba158449095d
sosohime <supersosohime@gmail.com>
2025-02-24 19:51:22 +0800

    fix: fix photo module

--- apps/backend-nest/src/bofans/photo/photo.module.ts
+++ apps/backend-nest/src/bofans/photo/photo.module.ts
@@ -3,9 +3,10 @@ import { PhotoController } from './photo.controller';
 import { PhotoService } from './photo.service';
 import { CategoryService } from '../category/category.service';
 import { PrismaService } from '@/library/prisma.service';
+import { UsersService } from '../users/users.service';
 
 @Module({
-  providers: [PrismaService, PhotoService, CategoryService],
+  providers: [PrismaService, UsersService, PhotoService, CategoryService],
   controllers: [PhotoController],
 })
 export class PhotoModule {}
