00f33a0bae4710c38652f4636f707fa22b8fdcd4
sosohime <supersosohime@gmail.com>
2025-02-24 17:15:54 +0800

    chore: update deps

--- apps/frontend-astro/tailwind.config.ts
+++ apps/frontend-astro/tailwind.config.ts
@@ -1,1 +1,21 @@
-export * from '@mono/config-tailwind';
+// export * from '@mono/config-tailwind';
+
+import type { Config } from 'tailwindcss';
+
+export default {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
