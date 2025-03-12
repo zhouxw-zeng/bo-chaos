a6c877ddca1c1c7d46fa3717755c6ab8d45af395
sosohime <supersosohime@gmail.com>
2025-03-12 18:55:20 +0800

    feat: add sentry

--- apps/frontend-astro/astro.config.mjs
+++ apps/frontend-astro/astro.config.mjs
@@ -1,9 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import tailwind from '@astrojs/tailwind';
+import sentry from '@sentry/astro';
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,6 +12,16 @@ export default defineConfig({
     react(),
     tailwind({
       applyBaseStyles: false,
+    }),
+    sentry({
+      dsn: 'https://55a75cceaf622a672fc7a5e79a265454@o4507808803782656.ingest.us.sentry.io/4508964321820672',
+      tracesSampleRate: 0,
+      replaysSessionSampleRate: 0,
+      replaysOnErrorSampleRate: 0,
+      sourceMapsUploadOptions: {
+        project: 'javascript-astro',
+        authToken: process.env.ASTRO_SENTRY_AUTH_TOKEN,
+      },
     }),
   ],
 });
