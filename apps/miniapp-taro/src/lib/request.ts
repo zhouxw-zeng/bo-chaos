99c4a4d4a8688daaa90bfbfb8bf08ce138ad70ba
sosohime <supersosohime@gmail.com>
2025-03-15 16:47:50 +0800

    build: revert config

--- apps/miniapp-taro/src/lib/request.ts
+++ apps/miniapp-taro/src/lib/request.ts
@@ -1,8 +1,8 @@
 import Taro from "@tarojs/taro";
 
 // 基础配置
-export const BASE_URL = "http://127.0.0.1:3000/bofans";
-// export const BASE_URL = "https://yuanbo.online/rpg/bofans";
+// export const BASE_URL = "http://127.0.0.1:3000/bofans";
+export const BASE_URL = "https://yuanbo.online/rpg/bofans";
 
 // 微信登录
 export const wxLogin = async () => {
